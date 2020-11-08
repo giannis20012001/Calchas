fix(calchas): Fix lyapunov exponents calculation.

misc/lyapunov_exponent_test_1.py
@@ -17,7 +17,10 @@ x = series.values
 x = x.astype('float32')
 # x = np.fromiter(series.values, dtype="float32")
 
-l = max(nolds.lyap_e(x))
-k = nolds.lyap_r(x, lag=None, min_tsep=None)
-print(l)
-print(k)
+emb_dim_ext = 3
+matrix_dim_ext = 3
+min_nb_ext = min(2 * matrix_dim_ext, matrix_dim_ext + 4)
+e = max(nolds.lyap_e(x, emb_dim=emb_dim_ext, matrix_dim=matrix_dim_ext, min_nb=min_nb_ext))
+r = nolds.lyap_r(x, emb_dim=emb_dim_ext, lag=None, min_tsep=None, min_neighbors=3, trajectory_len=3)
+print("Eckmann: " + str(e))
+print("Rosenstein: " + str(r))