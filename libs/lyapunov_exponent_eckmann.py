86f02c118b143ca934b3f4f1291bb61b8062a657
giannis20012001 <i.tsantilis@yahoo.com>
2021-01-30 18:30:08 +0200

    fix(calchas): Fix lyapunov exponents calculation.

---
+++ libs/lyapunov_exponent_eckmann.py
@@ -0,0 +1,29 @@
+import nolds
+from pandas import read_csv
+
+# lm = nolds.logistic_map(0.1, 1000, r=4)
+# x = np.fromiter(lm, dtype="float32")
+# l = max(nolds.lyap_e(x))
+# k = nolds.lyap_r(x)
+# print(l)
+# print(k)
+
+# load dataset
+series = read_csv('../data/datasets/ubuntu_application_server_final_week.csv',
+                  header=0, index_col=0, parse_dates=True, squeeze=True)
+x = series.values
+x = x.astype('float32')
+# x = np.fromiter(series.values, dtype="float32")
+
+# emb_dim_ext = 3
+# matrix_dim_ext = 3
+# min_nb_ext = min(2 * matrix_dim_ext, matrix_dim_ext + 4)
+# e = max(nolds.lyap_e(x, emb_dim=emb_dim_ext, matrix_dim=matrix_dim_ext, min_nb=min_nb_ext))
+# print("Eckmann: " + str(e))
+# r = nolds.lyap_r(x, emb_dim=emb_dim_ext, lag=None, min_tsep=None, min_neighbors=3, trajectory_len=3)
+
+emb_dim_ext = 5
+matrix_dim_ext = 5
+min_nb_ext = min(2 * matrix_dim_ext, matrix_dim_ext + 4)
+e = max(nolds.lyap_e(x, emb_dim=emb_dim_ext, matrix_dim=matrix_dim_ext, min_nb=min_nb_ext))
+print("result: " + str(e))
